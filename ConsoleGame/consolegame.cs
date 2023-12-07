2a1fca91015f2e1daaefb98c0c550e2a18d2a271
Uli <ulimorapro@gmail.com>
2023-12-06 19:01:12 -0600

    Initial Commit
    Initial Commit, got everything set up to start coding away at this game :)

---
+++ ConsoleGame/consolegame.cs
@@ -0,0 +1,18 @@
+ using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+
+ namespace ConsoleGame {
+     static class Program {
+
+         /// <summary>
+         /// The main entry point for the application.
+         /// </summary>
+         [STAThread]
+         static void Main() {
+            Console.WriteLine("Hi");
+         }
+     }
+
+ }
